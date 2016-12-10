0294738bc3015e32ed394645e7af24e2b94ac0f1
MiteSa <paloma.saporta@eleves.enpc.fr>
2016-12-10 17:31:03 +0100

    jeu nettoyé en fichiers séparés
    Je ne sais pas ce que sont les pycache, je les laisse au cas où..

---
+++ fichiers_separes/tools_player.py
@@ -0,0 +1,67 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Dec  7 13:26:43 2016
+
+@author: Paloma
+"""
+
+import tools
+      
+#-----------------------------------Classe Joueur
+class Player:
+    
+    #Initialisation
+    def __init__(self, name, value):
+        self._name=name
+        self.score=0 #correspond au nombre de positions occupées
+        self.used_positions=[] #a initialiser    .append marche! l.append((i,j))
+        self._value=value # valeur affectée au joueur
+        
+       
+    #@property 
+    
+    #Renvoie le nom du joueur  
+    def read_name(self):
+        return(self._name)
+        
+    #Renvoie le score du joueur
+    def read_score(self):
+        return (len(self.used_positions))
+        
+    #Renvoie la liste des positions occupées par le joueur sous forme d'une liste de doublets, chaque doublet étant le couple 
+    # (i,j) des indices des positions
+    def read_positions(self):
+        return(self.used_positions)
+    
+    #Renvoie la valeur affectée au joueur    
+    def read_value(self):
+        return (self._value)
+        
+        
+    #Modifie le score du joueur, en ajoutant le nombre entré en argument au score initial
+    def modify_score(self,add):
+        self.score+=add
+           
+    #Ajoute une position à la liste des positions occupées par le joueur
+    def occupy_position(self,i,j):
+        try:
+            self.used_positions.append((i,j)) #On ajoute la position à la liste des positions occupées
+            self.modify_score(1) #le score augmente de 1
+            
+        except tools.ErrorIndex: #Tritement de l'exception
+            print("Hors de la grille ....")
+    
+    def no_more_occupy_position(self,i,j):
+        try:
+            #verify_bounds(i,j,8)       #BOF ATTENTION ON FAIT DEJAINTERVENIR LA GRILLE... PPLUTOT A METTRE DANS LE JEU   
+            self.used_positions.remove((i,j)) #On ajoute la position à la liste des positions occupées
+            self.modify_score(-1) #le score augmente de 1
+            
+        except tools.ErrorIndex: #Tritement de l'exception
+            print("Hors de la grille ....")
+    
+        
+    #Vérifie si la position en argument est occupée par le joueur
+    def test_position(self,x,y):
+        return ((x,y) in self.used_positions)  #a tester
+        

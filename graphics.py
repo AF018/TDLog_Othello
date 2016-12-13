cc936453df44feafd2029c73b9a2c379f8519cf7
Antoine Francois <a.francois962@laposte.net>
2016-12-13 18:35:10 +0100

    affichage graphique du jeu, premiere version d'affichage des statistiques

--- graphics.py
+++ graphics.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from PyQt4 import QtCore, QtGui
+from csv import reader
 from utils import OthelloCell
 import tools
 from tools_game import Game
@@ -21,10 +22,16 @@ except AttributeError:
 
 class Ui_Window(object):
     def __init__(self):
+        """Récupère les statistiques pour pouvoir les modifier et lance un nouveau jeu"""
+        # Statistiques dans un fichier csv, lecture de chaque ligne du document pour récupérer
+        with open("stats.csv", newline = '') as text:
+            content = reader(text, delimiter = ',')
+            self.stats_tab=[[int(number) for number in line] for line in content]
         self.new_game()
 
     def setupUi(self):
         """Affiche tous les éléments de l'interface principale"""
+		# Création de tous les éléments de la fenêtre
         self.window = QtGui.QMainWindow()
         self.window.setObjectName(_fromUtf8("Window"))
         self.window.resize(480, 560)
@@ -53,6 +60,14 @@ class Ui_Window(object):
         self.score_2.setAlignment(QtCore.Qt.AlignCenter)
         self.score_2.setObjectName(_fromUtf8("score_2"))
         self.game_layout.addWidget(self.score_2, 8, 6, 1, 1)
+        self.information_1_label = QtGui.QLabel(self.gridLayoutWidget)
+        self.information_1_label.setAlignment(QtCore.Qt.AlignCenter)
+        self.information_1_label.setObjectName(_fromUtf8("information_1"))
+        self.game_layout.addWidget(self.information_1_label, 8, 3, 1, 1)
+        self.information_2_label = QtGui.QLabel(self.gridLayoutWidget)
+        self.information_2_label.setAlignment(QtCore.Qt.AlignCenter)
+        self.information_2_label.setObjectName(_fromUtf8("information_2"))
+        self.game_layout.addWidget(self.information_2_label, 8, 4, 1, 1)
         self.gridLayoutWidget.raise_()
         self.player_2.raise_()
         self.window.setCentralWidget(self.centralwidget)
@@ -78,7 +93,7 @@ class Ui_Window(object):
         self.menuOptions.addAction(self.action_stats)
         self.menubar.addAction(self.menuOthello.menuAction())
         self.menubar.addAction(self.menuOptions.menuAction())
-
+        # Création et affichage des boutons du plateau
         self.length = 8
         self.buttons = [[OthelloCell(self.game.grid.read_element(i,j))
             for i in range(self.length)]
@@ -86,16 +101,23 @@ class Ui_Window(object):
         for i in range(self.length):
             for j in range(self.length):
                 self.game_layout.addWidget(self.buttons[i][j],i,j)
-
+        # Affichage de la fenêtre de jeu
         self.window.show()
 
     def retranslateUi(self):
         """Affiche les textes de l'interface principale"""
+        self.window.setStyleSheet("QMainWindow {background: 'darkgrey';}")
         self.window.setWindowTitle("Othello")
+        self.player_1.setStyleSheet("QLabel { color : black; }")
         self.player_1.setText(self.game.player1.read_name())
-        self.score_1.setText(str(0))
+        self.score_1.setStyleSheet("QLabel { color : black; }")
+        self.score_1.setText(str(self.game.player1.read_score()))
+        self.player_2.setStyleSheet("QLabel { color : white; }")
         self.player_2.setText(self.game.player2.read_name())
-        self.score_2.setText(str(0))
+        self.score_2.setStyleSheet("QLabel { color : white; }")
+        self.score_2.setText(str(self.game.player2.read_score()))
+        self.information_1_label.setText("Au tour de :")
+        self.information_2_label.setText(self.game.player1.read_name())
         self.menuOthello.setTitle("Game")
         self.menuOptions.setTitle("Options")
         self.action_new_game.setText("New Game")
@@ -104,7 +126,6 @@ class Ui_Window(object):
 
     def start(self):
         """Gère les événements : boutons cliqués, signaux émis"""
-        print("pet")
         playable_pos = self.game.valid_positions(self.game.current_player)[1]
         for i in range(self.length):
             for j in range(self.length):
@@ -128,23 +149,31 @@ class Ui_Window(object):
             origins=self.game.origins(i,j,self.game.current_player)
             # Commande permettant de jouer le mouvement proposé
             self.game.play_one_shot(i,j,self.game.current_player)
-            # Effectue tous les retournements engendrés par le coup
+            # Effectue tous les retournements de pion engendrés par le coup
             self.game.turn_pawn(i,j,self.game.current_player,*origins)
-            print(" ")
-            self.game.display()
+			# Changement de joueur
+            self.game.current_player=self.game.opponent(self.game.current_player)
+			# Calcul de toutes les positions jouables dans playable_pos
+            playable_pos = self.game.valid_positions(self.game.current_player)[1]
+            if len(playable_pos)==0:
+                # Changement de joueur si celui dont c'est le tour n'a pas de position admissible
+                self.game.current_player=self.game.opponent(self.game.current_player)
+                playable_pos = self.game.valid_positions(self.game.current_player)[1]
+            self.information_2_label.setText(self.game.current_player.read_name())
+            self.score_1.setText(str(self.game.player1.read_score()))
+            self.score_2.setText(str(self.game.player2.read_score()))
             for k in range(self.length):
                 for l in range(self.length):
-                    self.buttons[k][l].refresh_color(self.game.grid.read_element(k,l))
-            print("PROUT")
-            if len(self.game.valid_positions(self.game.current_player)[1])>0:
-                self.game.current_player=self.game.opponent(self.game.current_player)
-            print("REPROUT")
-			# BLA BLA BLA
-			# LIRE LIRE.txt
-        #self.start()
-
-        #PROBLEME LA LISTE N4EST PAS ACTUALIS2E IL FAUT L4ACTUALISER
-		#PAREIL PLATEAU §§§§§§§§§§§§§§§§§§§§§§§
+                    self.buttons[k][l].refresh(self.game.grid.read_element(k,l),(k,l) in playable_pos)
+            if self.game.end_game():
+                self.information_1_label.setText("Gagnant :")
+                self.information_2_label.setText("")
+                #créer une fonction pour afficher la fin de partie
+                #faire quelque chose pour la fin du jeu : affichage des stats ou je sais pas quoi 
+                #probleme sur certaines cases au relancement d'une partie
+				#checker le vainqueur a la fin du jeu
+				#mettre a jour les statistiques une fois la partie finie en ecrasant le fichier
+                #reorganiser les fonctions et autres pour plus de lisibilité
         return
 
     def cell_test(self,i,j):
@@ -162,6 +191,7 @@ class Ui_Window(object):
         # Les objets ne sont pas des attributs de la classe car ils n'ont pas
         # vocation à exister hors de cette fonction
         self.new_game_window = QtGui.QMainWindow()
+        self.new_game_window.setStyleSheet("QMainWindow {background: 'darkgrey';}")
         central_widget = QtGui.QWidget(self.new_game_window)
         self.new_game_window.setCentralWidget(central_widget)
         layout = QtGui.QGridLayout()
@@ -178,7 +208,7 @@ class Ui_Window(object):
         layout.addWidget(AI_box,2,0)
         start_button = QtGui.QPushButton("Commencer")
         layout.addWidget(start_button,3,3)
-        self.new_game_window.setWindowTitle("New Game")
+        self.new_game_window.setWindowTitle("Nouvelle partie")
         self.new_game_window.show()
         start_button.clicked.connect(lambda:self.set_parameters(player_1_name.text(),player_2_name.text()))
 
@@ -187,22 +217,27 @@ class Ui_Window(object):
         # Les objets ne sont pas des attributs de la classe car ils n'ont pas
         # vocation à exister hors de cette fonction
         self.stats_window = QtGui.QMainWindow()
+        self.stats_window.setStyleSheet("QMainWindow {background: 'darkgrey';}")
         central_widget = QtGui.QWidget(self.stats_window)
         self.stats_window.setCentralWidget(central_widget)
         layout = QtGui.QGridLayout()
         central_widget.setLayout(layout)
-        player_1_label = QtGui.QLabel("En construction")
-        layout.addWidget(player_1_label,0,0)
+        game_nb_label = QtGui.QLabel("Nombre de parties sans IA :")
+        layout.addWidget(game_nb_label,0,0)
+        game_nb = QtGui.QLabel(str(self.stats_tab[0][0]))
+        layout.addWidget(game_nb,0,1)
+        AI_game_nb_label = QtGui.QLabel("Nombre de parties avec IA :")
+        layout.addWidget(AI_game_nb_label,1,0)
+        AI_game_nb = QtGui.QLabel(str(self.stats_tab[1][0]))
+        layout.addWidget(AI_game_nb,1,1)
         exit_button = QtGui.QPushButton("Continuer")
-        layout.addWidget(exit_button,1,1)
+        layout.addWidget(exit_button,2,2)
         self.stats_window.setWindowTitle("Statistics")
         self.stats_window.show()
         self.stats_window.connect(exit_button, QtCore.SIGNAL('clicked()'), lambda:self.stats_window.close())
 
     def ok(self):
         print("Ok")
-
-### Permettre la lecture ecriture du fichier de statistiques
 
 import sys
 app = QtGui.QApplication(sys.argv)
--- utils.py
+++ utils.py
@@ -14,14 +14,21 @@ class OthelloCell(QtGui.QLabel):
             self.setPixmap(QtGui.QPixmap('cell.jpg'))
         self.setMouseTracking(True)
 
-    def refresh_color(self,new_color_nb):
+    def refresh(self,new_color_nb,is_playable):
+        """Actualise l'apparence de la case et l'attribut playable permettant de savoir
+        si la case est jouable"""
         self.color_nb=new_color_nb
         if (self.color_nb == 1):
             self.setPixmap(QtGui.QPixmap('occ_white_cell.jpg'))
         elif (self.color_nb == -1):
             self.setPixmap(QtGui.QPixmap('occ_black_cell.jpg'))
+        if is_playable:
+            self.playable = 1
+        else:
+            self.playable = 0
 
     def mouseReleaseEvent(self, event):
+        print(self.playable)
         self.emit(QtCore.SIGNAL('clicked()'))
 
     def enterEvent(self,event):
